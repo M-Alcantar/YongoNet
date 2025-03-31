a7d138eb2714470eda430a6c744f5d0eebb8a261
M-Alcantar <129239276+M-Alcantar@users.noreply.github.com>
2025-03-30 19:18:24 -0700

    First steps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import dinoImg from '$lib/assets/dinosaur.jpg';
+
+    let showDino = false;
+</script>
+
+<svelte:head>
+    <title>I just wanna make a button</title>
+</svelte:head>
+
+<!-- svelte-ignore a11y_consider_explicit_label -->
+<button
+    style="width:20%;"
+    class="absolute top-36 left-36 rounded-lg py-2"
+    class:bg-red-300={showDino}
+    class:bg-green-300={!showDino}
+    on:click=
+        {
+            () => {showDino = !showDino}
+        }
+>
+    {#if showDino}
+        Nvm I'm scared
+    {:else}
+        Ok I wanna see a cool dinosaur
+    {/if}
+</button>
+
+<img src={dinoImg} alt="RAAAAAAARRRRRRRRRRRR" 
+    style="height:500px;" 
+    class="absolute top-12 right-50" 
+    hidden={!showDino} />
