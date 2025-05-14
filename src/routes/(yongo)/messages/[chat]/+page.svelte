4d52a3cf4e0e70ec24216342cc19047638a22fa7
M-Alcantar <129239276+M-Alcantar@users.noreply.github.com>
2025-05-13 19:21:57 -0700

    Message wip

--- src/routes/(yongo)/messages/[chat]/+page.svelte
+++ src/routes/(yongo)/messages/[chat]/+page.svelte
@@ -5,8 +5,19 @@
 </script>
 
 <div class="flex flex-col h-full w-full px-5 py-3">
-    <div class="flex-1">
-        <p class="text-white text-center">{JSON.stringify(data.messages?.at(0))}</p>
+    <div class="flex flex-col flex-1 w-full min-w-md ">
+        {#if data.messages}
+            {#each data.messages as message}
+                <div class="w-auto mb-2 align-middle">
+                    <span class="p-1 -ml-1 bg-[#03142b] text-sm text-white rounded-sm align-middle">{message.sender}</span>
+                    {#if message.message.text !== ""}
+                        <p class="w-full text-white text-md" title={new Date(message.datetime * 1000).toLocaleString()}>
+                            {message.message.text}
+                        </p>
+                    {/if}
+                </div>
+            {/each}
+        {/if}
     </div>
-    <div class="flex-none h-1/15 bg-[#cccccc] w-full">Inputi Hari poti</div>
+    <div class="flex flex-none w-full rounded-md h-1/15 bg-[#969696]/40 text-white items-center px-3">Send a massage...</div>
 </div>
